e49f4052410aae0995d1a809d00ed64f67adadb7
rerost <gmailii@yahoo.co.jp>
2019-02-10 01:46:23 +0900

    Pass real value

--- app/run.go
+++ app/run.go
@@ -4,14 +4,25 @@ import (
 	"github.com/rerost/chaos-pubsub/app/server"
 	"github.com/rerost/chaos-pubsub/grpcserver"
 	"github.com/srvc/fail"
+	api_pb "google.golang.org/genproto/googleapis/pubsub/v1"
+	"google.golang.org/grpc"
 )
 
 // Run starts the grapiserver.
 func Run() error {
+	conn, err := grpc.Dial("localhost:8085", grpc.WithInsecure())
+	if err != nil {
+		return fail.Wrap(err)
+	}
+	defer conn.Close()
+
+	publisherClient := api_pb.NewPublisherClient(conn)
+	subscriberClient := api_pb.NewSubscriberClient(conn)
+
 	s := grpcserver.New(
 		grpcserver.WithServers(
-			server.NewPublisherServiceServer(nil),
-			server.NewSubscriberServiceServer(nil),
+			server.NewPublisherServiceServer(publisherClient),
+			server.NewSubscriberServiceServer(subscriberClient),
 		),
 	)
 	return fail.Wrap(s.Serve())
